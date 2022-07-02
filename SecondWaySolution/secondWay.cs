887cba8709e0bb16f0953eafdc35878feb37ce34
Anton Sineev <sineevanton@mail.ru>
2022-07-02 20:33:20 +0300

    Second way solution completed

---
+++ SecondWaySolution/secondWay.cs
@@ -0,0 +1,15 @@
+﻿string[] arr = new string[]{"this", "is", "some", "text", "string", "and", "a", "ssh"}; 
+string str = ""; 
+foreach (var word in arr) 
+{ 
+    if (word.Length <= 3) 
+    { 
+        str += word + " "; 
+    } 
+} 
+ 
+var answer = str.Trim().Split(' ').ToArray(); 
+ 
+Console.WriteLine(answer); 
+ 
+Console.WriteLine("[{0}]", string.Join(", ", answer));
