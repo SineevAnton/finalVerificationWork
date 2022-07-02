444872f73089c4915f98437412f5047e36af8437
Anton Sineev <sineevanton@mail.ru>
2022-07-02 20:31:09 +0300

    First way solution completed

---
+++ FirstWaySolution/firstWay.cs
@@ -0,0 +1,24 @@
+﻿string[] arr = new string[]{"this", "is", "some", "text", "string", "and", "a", "ssh"}; 
+int answerArrLength = 0;
+
+foreach (var word in arr)
+{
+    if (word.Length <= 3)
+    {
+        answerArrLength += 1;
+    }
+}
+
+string[] answerArr = new string[answerArrLength];
+int answerIndex = 0;
+
+foreach (var word in arr)
+{
+    if (word.Length <= 3)
+    {
+        answerArr[answerIndex] = word;
+        answerIndex += 1;
+    }
+}
+
+Console.WriteLine("[{0}]", string.Join(", ", answerArr));
